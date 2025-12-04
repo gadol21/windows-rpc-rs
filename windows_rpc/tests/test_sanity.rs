535fef4b5af8b1f078d72eb681072882135f5dca
Gadol21 <gadol21dev@gmail.com>
2025-12-04 08:01:22 +0200

    Auto generated interface from trait

--- windows_rpc/tests/test_sanity.rs
+++ windows_rpc/tests/test_sanity.rs
@@ -1,16 +1,16 @@
-use windows_rpc_macros::gen_interface;
+use windows_rpc::client_binding::ProtocolSequence;
+use windows_rpc_macros::rpc_interface;
 
-gen_interface!();
+#[rpc_interface(guid(0x7a98c250_6808_11cf_b73b_00aa00b677a7), version(0.0))]
+trait TestRpcIface {
+    fn NoParams() -> u64;
+    fn SingleParamReturn(param: i32) -> i32;
+    fn Sum(a: i32, b: i32) -> i32;
+}
 
 #[test]
 fn test() {
-    let client = Hello::new(
-        ClientBinding::new(
-            windows_rpc::client_binding::ProtocolSequence::Alpc,
-            "foobar",
-        )
-        .unwrap(),
-    );
+    let client = TestRpcIface::new(ClientBinding::new(ProtocolSequence::Alpc, "foobar").unwrap());
 
     assert_eq!(client.NoParams(), 0xffffffff + 1);
     assert_eq!(client.SingleParamReturn(10), 14);
--- windows_rpc_macros/src/lib.rs
+++ windows_rpc_macros/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
-use quote::{format_ident, quote};
-use syn::Ident;
+use quote::{ToTokens, format_ident, quote};
+use syn::{FnArg, Ident, ReturnType, TraitItem, Type as SynType};
 use windows::core::GUID;
 
 #[allow(non_upper_case_globals)]
@@ -94,6 +94,45 @@ enum Type {
     //Pointer(Box<Type>),
     String,
     Simple(BaseType),
+}
+
+impl TryFrom<SynType> for Type {
+    type Error = syn::Error;
+
+    fn try_from(value: syn::Type) -> Result<Self, Self::Error> {
+        let SynType::Path(path) = &value else {
+            return Err(syn::Error::new_spanned(
+                value.to_token_stream(),
+                "Only functions are allowed on this trait",
+            ));
+        };
+        let ident = path.path.require_ident()?;
+        // FIXME: for each enum variant?
+        let res = if ident == "u8" {
+            Self::Simple(BaseType::U8)
+        } else if ident == "i8" {
+            Self::Simple(BaseType::I8)
+        } else if ident == "i16" {
+            Self::Simple(BaseType::I16)
+        } else if ident == "u16" {
+            Self::Simple(BaseType::U16)
+        } else if ident == "i32" {
+            Self::Simple(BaseType::I32)
+        } else if ident == "u32" {
+            Self::Simple(BaseType::U32)
+        } else if ident == "i64" {
+            Self::Simple(BaseType::I64)
+        } else if ident == "u64" {
+            Self::Simple(BaseType::U64)
+        } else {
+            return Err(syn::Error::new_spanned(
+                ident.to_token_stream(),
+                "Unsupported type was used",
+            ));
+        };
+
+        Ok(res)
+    }
 }
 
 impl Type {
@@ -862,6 +901,98 @@ fn compile_client(interface: Interface) -> proc_macro2::TokenStream {
             #(#methods)*
         }
     }
+}
+
+// FIXME: simplify by extracting to method that return Result<proc_macro2::TokenStream, Error>
+#[proc_macro_attribute]
+pub fn rpc_interface(
+    attr: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input_clone = input.clone();
+    let t = match syn::parse2(input.into()) {
+        Err(e) => {
+            return e.to_compile_error().into();
+        }
+        Ok(syn::Item::Trait(t)) => t,
+        Ok(unrecognized) => {
+            return syn::Error::new_spanned(
+                &unrecognized,
+                "The #[trace_logging_provider] attribute cannot be used with this kind of item.",
+            )
+            .to_compile_error()
+            .into();
+        }
+    };
+
+    let mut methods = vec![];
+    for item in t.items {
+        let TraitItem::Fn(func) = item else {
+            return syn::Error::new_spanned(
+                proc_macro2::TokenStream::from(input_clone),
+                "Only functions are allowed on this trait",
+            )
+            .into_compile_error()
+            .into();
+        };
+
+        let return_type = match func.sig.output {
+            ReturnType::Default => None,
+            ReturnType::Type(_, t) => match Type::try_from(*t) {
+                Ok(t) => Some(t),
+                Err(e) => return e.into_compile_error().into(),
+            },
+        };
+
+        let mut params = vec![];
+        for param in func.sig.inputs {
+            let FnArg::Typed(typed) = param else {
+                // FIXME: I'd like to support that (and even make that mandatory)
+                return syn::Error::new_spanned(
+                    proc_macro2::TokenStream::from(input_clone),
+                    "Passing self is currently not supported",
+                )
+                .into_compile_error()
+                .into();
+            };
+
+            let syn::Pat::Ident(param_name) = *typed.pat else {
+                return syn::Error::new_spanned(typed.pat.to_token_stream(), "Expected identifier")
+                    .into_compile_error()
+                    .into();
+            };
+
+            let param_type = match Type::try_from(*typed.ty) {
+                Ok(t) => t,
+                Err(e) => return e.into_compile_error().into(),
+            };
+
+            params.push(Parameter {
+                r#type: param_type,
+                name: param_name.ident.to_string(),
+                // FIXME: let mut affect this (can be in/out)
+                is_in: true,
+                is_out: false,
+            });
+        }
+
+        methods.push(Method {
+            return_type,
+            name: func.sig.ident.to_string(),
+            parameters: params,
+        });
+    }
+
+    let interface = Interface {
+        name: t.ident.to_string(),
+        // FIXME: get this as attribute
+        uuid: GUID::from_u128(0x7a98c250_6808_11cf_b73b_00aa00b677a7),
+        // FIXME: get this as attribute
+        version: InterfaceVersion { major: 0, minor: 0 },
+        methods,
+    };
+
+    compile_client(interface).into()
 }
 
 #[proc_macro]
